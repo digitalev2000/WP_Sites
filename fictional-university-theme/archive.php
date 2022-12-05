6e06b05faa0ed38846ea79b85c1ef107a4fd5cb1
Richard Berry <berry.richard1@gmail.com>
2022-12-04 22:05:56 -0600

    This is the custom theme file ...
    This is the custom theme file ...

---
+++ fictional-university-theme/archive.php
@@ -0,0 +1,52 @@
+<?
+get_header(); 
+?>
+
+<div class="page-banner">
+    <!-- Header Banner bg section -->
+
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">
+        <?php
+            the_archive_title();
+        ?>
+        </h1>
+        <div class="page-banner__intro">
+          <p><? 
+          the_archive_description(); 
+          ?></p>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="container container--narrow page-section">
+    <?php
+    while(have_posts()){
+        the_post(); ?>
+
+    <div class="post-item">
+
+        <h2 class="headline headline--small headline--post-title"><a href="<? the_permalink(); ?>"><? the_title(); ?></a></h2>
+
+        <div class="metabox">
+            <p>Posted By: <? the_author_posts_link(); ?> on <? the_time('n.j.y'); ?> in <?  echo get_the_category_list(', '); ?></p>
+        </div>
+
+        <div class="generic-content">
+            <? the_excerpt(); ?>
+            <p><a class="btn btn--blue" href="<? the_permalink(); ?>">Continue Reading &raquo;</a></p>
+        </div>
+
+    </div>
+
+   <!-- | Pagination Script | -->
+    <?php }
+      echo paginate_links();  
+    ?>
+    </div>
+
+<? 
+get_footer();
+?>
